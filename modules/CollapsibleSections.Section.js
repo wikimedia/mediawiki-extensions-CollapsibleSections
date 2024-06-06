05522d50c4677be4e7805fb889ff646af115a7cd
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-06-06 03:26:20 +0000

    build: Updating npm dependencies
    * eslint-config-wikimedia: 0.27.0 → 0.28.0
    * grunt-stylelint: 0.19.0 → 0.20.0
    * stylelint-config-wikimedia: 0.16.1 → 0.17.1
    
    Change-Id: Ib2dea5485ace3bdeae6e5bb04f8578380adb7a3f

--- modules/CollapsibleSections.Section.js
+++ modules/CollapsibleSections.Section.js
@@ -35,7 +35,7 @@
 		this.$content = null;
 		var currentHeading = parseInt( mw.config.get( 'wgCollapsibleSectionsTag' ).charAt( 1 ) );
 		// Find and wrap all contents until next section (any heading!)
-		// eslint-disable-next-line no-jquery/no-sizzle,no-jquery/variable-pattern
+		// eslint-disable-next-line no-jquery/variable-pattern
 		elements = this.$element.nextUntil( function () {
 			return parseInt( this.tagName.charAt( 1 ) ) <= currentHeading;
 		} );
